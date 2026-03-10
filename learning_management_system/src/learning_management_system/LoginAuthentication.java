14da1c65de3cebcfd036534ad81556ad43e29aa0
haroonahmad256 <ha9417293@gmail.com>
2026-03-10 19:57:16 +0500

    Added Forgot Password Method and Updated CSV files Data

--- learning_management_system/src/learning_management_system/LoginAuthentication.java
+++ learning_management_system/src/learning_management_system/LoginAuthentication.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.Scanner;
 
 public class LoginAuthentication {
     private String userName;
@@ -89,5 +90,61 @@ public class LoginAuthentication {
         return access;
     }
     //-----------------------------------------
+    static void forgotPassword(){
+        String emailAddress, userName;
+        String Line;
+        String newPassword;
+        boolean access= false;
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("Password Reset...");
+        System.out.print("Enter your student id: ");
+        userName= scanner.nextLine();
+        System.out.print("Enter your Email address: ");
+        emailAddress= scanner.nextLine();
+        try(BufferedReader reader= new BufferedReader(new FileReader("Students.csv"))){
+            FileWriter writer= new FileWriter("Students_temp.csv");
+            while((Line=reader.readLine())!=null){
+                String[] lineArray= Line.split(",");
+                if(lineArray[0].equals(userName) && lineArray[3].equals(emailAddress)){
+                    System.out.println("Email Id confirmed");
+                    System.out.print("Enter your new Password: ");
+                    newPassword= scanner.nextLine();
+                    writer.write(lineArray[0]+","+newPassword+","+lineArray[2]+","+lineArray[3]+","+lineArray[4]+","+lineArray[5]+"\n");
+                    access= true;
+                }
+                else{
+                    writer.write(Line+"\n");
+                }
+            }
+            reader.close();
+            writer.close();
+            File oldfile= new File("Students.csv");
+            File newFile= new File("Students_temp.csv");
 
+            if(access){
+                if(oldfile.delete()){
+                    if(!newFile.renameTo(oldfile)){
+                        System.out.println("File couldn't Renamed");
+                    }
+                }
+                else{
+                    System.out.println("File couldn't be deleted");
+                }
+            }
+            else{
+                if(newFile.delete()){
+                    System.out.println("Password couldn't be deleted");
+                }
+                else{
+                    System.out.println("File couldn't be deleted");
+                }
+            }
+        }
+        catch(FileNotFoundException e){
+            System.out.println("File Not Found");
+        }
+        catch(IOException e){
+            System.out.println("Something Error Happened");
+        }
+    }
 }
--- learning_management_system/src/learning_management_system/Main.java
+++ learning_management_system/src/learning_management_system/Main.java
@@ -4,49 +4,80 @@ import java.util.TimerTask;
 
 public class Main {
     public static void main(String[] args) {
-        int count=0;
-        System.out.println("Welcome To Learning Management System");
-        do{
-            String userName, Password;
-            Scanner scanner= new Scanner(System.in);
-            System.out.print("Enter your user name: ");
-            userName= scanner.nextLine();
-            System.out.print("Enter your password: ");
-            Password= scanner.nextLine();
-            LoginAuthentication verifyUser= new LoginAuthentication(userName, Password);
+        int count = 0;
+        int choice = -1;
+        String userName=null, Password;
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.println("Welcome To Learning Management System");
+            System.out.println("1. Sign in");
+            System.out.println("2. Forgot Password");
+            System.out.println("3. Exit");
+            System.out.print("Enter your choice: ");
+            choice = scanner.nextInt();
+            scanner.nextLine();
+            switch (choice) {
+                case 1: {
+                    do{
+                        System.out.println("Enter Your Credentials to Login");
+                        System.out.print("Enter your user name: ");
+                        userName = scanner.nextLine();
+                        System.out.print("Enter your password: ");
+                        Password = scanner.nextLine();
+                        LoginAuthentication verifyUser = new LoginAuthentication(userName, Password);
 
-            if(verifyUser.verifyStudent()){
-                break;
-            }
-            else if(verifyUser.verifyTeacher()){
-                break;
-            }
-            else if(verifyUser.verifyAdmin()){
-                break;
-            }
-            else{
-                System.out.println("Invalid Username or Password");
-                count++;
-            }
-            if(count==4){
-                System.out.println("*You have tried for 4 times*");
-                System.out.println("*Wait 30 seconds to try again*");
-                Timer timer = new Timer();
-                TimerTask timerTask= new TimerTask() {
-                    int counter= 31;
-                    @Override
-                    public void run() {
-                        counter--;
-                        System.out.printf("\r*Timer: %d*", counter);
-                        if(counter ==  0){
-                            System.out.println("\nPress Enter to Continue...");
-                            timer.cancel();
+                        if (verifyUser.verifyStudent()) {
+                            break;
+                        } else if (verifyUser.verifyTeacher()) {
+                            break;
+                        } else if (verifyUser.verifyAdmin()) {
+                            break;
+                        } else {
+                            System.out.println("Invalid Username or Password");
+                            count++;
                         }
+                        if (count == 4) {
+                            System.out.println("*You have tried for 4 times*");
+                            System.out.println("*Wait 30 seconds to try again*");
+                            Timer timer = new Timer();
+                            TimerTask timerTask = new TimerTask() {
+                                int counter = 31;
+
+                                @Override
+                                public void run() {
+                                    counter--;
+                                    System.out.printf("\r*Timer: %d*", counter);
+                                    if (counter == 0) {
+                                        System.out.println("\nPress Enter to Continue...");
+                                        timer.cancel();
+                                    }
+                                }
+                            };
+                            timer.scheduleAtFixedRate(timerTask, 0, 1000);
+                            scanner.nextLine();
+                        }
+                    }while (userName!=null);
+
+                    break;
+                }
+                case 2: {
+                        LoginAuthentication.forgotPassword();
+                    break;
+                }
+                case 3: {
+                    System.out.println("Exiting...");
+                    try {
+                        Thread.sleep(5000);
+                    } catch (InterruptedException e) {
+                        System.out.println("Error Encountered");
                     }
-                };
-                timer.scheduleAtFixedRate(timerTask, 0, 1000);
-                scanner.nextLine();
+                    System.out.println("See You Soon...");
+                    break;
+                }
+                default:
+                    break;
             }
-        }while(true);
+        } while (choice != 3);
+        scanner.close();
     }
 }
